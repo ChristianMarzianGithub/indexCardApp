617935459a74d29aae6ae923ce0d1104b1b3ca4d
Christian Marzian <christian.marzian@adesso.de>
2024-04-16 09:05:56 +0200

    updated

--- indexcardfrontend/src/components/IndexCardAppComponent.jsx
+++ indexcardfrontend/src/components/IndexCardAppComponent.jsx
@@ -5,6 +5,10 @@ function IndexCardAppComponent(){
 
     const[startingIndex, setStartingIndex] = useState(0)
     const[showAnswer, setShowAnswer] = useState(false)
+    const[selectedCategory, setSelectedCategory] = useState(0)
+    const[categoryList, setCategoryList] = useState([])
+    const[indexCardList, setIndexCardList] = useState([]);
+    const[filteredIndexCardList, setFilteredIndexCardList] = useState([]);
 
     function switchShowAnswer(){
         if(showAnswer){
@@ -34,25 +38,39 @@ function IndexCardAppComponent(){
         setShowAnswer(false)
     }
 
-    const [indexCardList, setIndexCardList] = useState([]);
+    
 
 
     useEffect(() => {
         retrieveAllIndexCards();
     }, []);
+
+    useEffect(() => {
+        retrieveAllCategories();
+    }, []);
     
-    function successfullResponse(data, setIndexCardList){
+    function successfullResponseIndexCards(data, setIndexCardList){
         setIndexCardList(data);
     
         console.log(indexCardList);
     }
 
-    function retrieveAllIndexCards(){
-        
-        axios.get('http://localhost:8080/indexcard/getAllIndexCard')
-            .then( (response) => successfullResponse(response.data, setIndexCardList) )
-        
+    function successfullResponseCategories(data, setCategoryList){
+        setCategoryList(data);
+    
+        console.log(categoryList);
     }
+
+    function retrieveAllIndexCards(){
+        axios.get('http://localhost:8080/indexcard/getAllIndexCard')
+            .then( (response) => successfullResponseIndexCards(response.data, setIndexCardList) )
+    }
+
+    function retrieveAllCategories(){
+        axios.get('http://localhost:8080/category/getAllCategories')
+            .then( (response) => successfullResponseCategories(response.data, setCategoryList) )
+    }
+
 
     function getLanguageTwo(){
         if(showAnswer){
@@ -64,20 +82,28 @@ function IndexCardAppComponent(){
         }
     }
 
+    const filterIndexCardList = (event) => {
+
+        setFilteredIndexCardList(indexCardList.filter(item => item.category.id ===  parseInt(event.target.value) ))
+        console.log(filterIndexCardList)
+    }
+
     return(
         <div>
             <h1>Karteikarten</h1>
             <br/>
+            <button onClick={retrieveAllCategories}>retrieveCategories</button>
             <br/>
+            <select onChange={filterIndexCardList} value={selectedCategory}>
+            {categoryList.map(item => (
+                <option key={item.id} value={item.id}>{item.description}</option>
+            ))}
+            </select>
             <br/>
             <button onClick={backToStart}>zum Anfang</button><button onClick={decreaseStartingIndex}>zurück</button><button onClick={increaseStartingIndex}>weiter</button>
             <p>{indexCardList.length > 0 && indexCardList[startingIndex].question}</p>
                 <p>---------------</p>
                 {getLanguageTwo()}
-                
-            
-            
-            
         </div>
     )
 }
